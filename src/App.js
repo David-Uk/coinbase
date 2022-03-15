a82d096d841398f8584519dd2c5868809687961d
David Ukelere <david.ukelere291@gmail.com>
2022-03-15 10:59:35 +0100

    Added tables

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Coins from './components/Coins';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [coins,setCoins] = useState([]);
+  const [coins, setCoins] = useState([]);
 
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(url)
-    .then(res => {
-      setCoins(res.data)
-      console.log(res.data[0])})
-    .catch(err => console.log(err))
-  },[])
+      .then(res => {
+        setCoins(res.data)
+        console.log(res.data[0])
+      })
+      .catch(err => console.log(err))
+  }, [])
   return (
     <>
-      <Coins coins={coins}/>
+      <Navbar />
+      <Coins coins={coins} />
     </>
   );
 }
